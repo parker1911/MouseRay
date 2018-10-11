afeb82f3ea0fa493ae66351ec537b5468166c7b9
Thomas Parker <parkert1911@gmail.com>
2018-10-10 18:42:38 -0700

    Testing

--- MouseRay/Main.cpp
+++ MouseRay/Main.cpp
@@ -11,7 +11,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "MouseRay Test!", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "MouseRay Test", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
